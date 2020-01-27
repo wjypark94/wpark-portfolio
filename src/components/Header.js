43e56f85858b7f72321777ae05899fe770f650d3
William Park <williampark@Williams-MBP-2.lan1>
2020-01-26 16:50:41 -0800

    added fade to header and nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react';
 import '../componentStyles/Header.css';
+import Scroll from 'react-scroll';
+import Fade from 'react-reveal/Fade';
 
 class Header extends Component {
 	render() {
+		let Link = Scroll.Link;
 		return (
 			<div className="container pt-5">
 				<div className="row">
-					<div className="col py-3 header-title">
+					<Fade bottom>
+					<div className="col-12 py-3 header-title">
 						<h2>Hi, my name is Will Park and</h2>
 						<h2>I'm a front end web developer.</h2>
+					</div>
+					</Fade>
+					<div class="col-12 pb-3">
 						<a href="/">View recent work</a>
 					</div>
 				</div>
-				
+				<Fade bottom>
+					<div class="row">
+						<div className="col-12 arrow-wrapper w-100">
+							<Link to="about" offset={-40} spy={true} smooth={true} duration={700}>
+							<i className="fa fa-chevron-down smooth-container" aria-hidden="true" ></i>
+							</Link>
+						</div>
+					</div>
+				</Fade>
+
 			</div>
 
 		)
