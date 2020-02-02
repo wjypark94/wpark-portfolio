e4ab3ce1ecda4539e5665f5d8007de79b9f6cbf1
William Park <williampark@Williams-MacBook-Pro-2.local>
2020-02-01 18:02:33 -0800

    added more basic layout to recent work component

--- src/components/RecentWork.js
+++ src/components/RecentWork.js
@@ -4,17 +4,41 @@ import '../componentStyles/RecentWork.css';
 class RecentWork extends Component {
 	render(){
 		return (
-			<div className="container">
+			<div className="container py-5">
 				<div className="row">
 					<div className="col-12 text-left pl-0 py-3">
-						<h2>Recent Work</h2>
+						<h2 class="work-header text-uppercase">Recent Work</h2>
 					</div>
 				</div>
-				<div className="row work-border py-5">
-					<div className="col-6">
+				<div className="row work-border py-5 mb-5">
+					<div className="col-12 col-lg-6">
 						Product
 					</div>
-					<div className="col-6">
+					<div className="col-12 col-lg-6">
+						Product description
+					</div>
+				</div>
+				<div className="row work-border py-5 mb-5">
+					<div className="col-12 col-lg-6">
+						Product
+					</div>
+					<div className="col-12 col-lg-6">
+						Product description
+					</div>
+				</div>
+				<div className="row work-border py-5 mb-5">
+					<div className="col-12 col-lg-6">
+						Product
+					</div>
+					<div className="col-12 col-lg-6">
+						Product description
+					</div>
+				</div>
+				<div className="row work-border py-5 mb-5">
+					<div className="col-12 col-lg-6">
+						Product
+					</div>
+					<div className="col-12 col-lg-6">
 						Product description
 					</div>
 				</div>
