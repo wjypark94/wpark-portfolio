bf2e539ce8d0e661d459b3257b905f5f36bf076d
William Park <williampark@Williams-MacBook-Pro-2.local>
2020-02-05 17:45:37 -0800

    added more styles to recent work component

--- src/components/PastProjects.js
+++ src/components/PastProjects.js
@@ -10,6 +10,30 @@ class PastProjects extends Component{
 						<h2 class="work-header text-uppercase">Past Projects</h2>
 					</div>
 				</div>
+
+				<div class="row mx-0 mx-lg-5 mb-5">
+					<div class="col-6">
+						<div class="row work-border">
+							<div class="col-12">
+								App
+							</div>
+							<div class="col-12">
+								App description
+							</div>
+						</div>
+					</div>
+					<div class="col-6">
+						<div class="row work-border">
+							<div class="col-12">
+								App
+							</div>
+							<div class="col-12">
+								App description
+							</div>
+						</div>
+					</div>
+					
+				</div>
 			</div>
 		)
 	}
--- src/components/RecentWork.js
+++ src/components/RecentWork.js
@@ -14,42 +14,42 @@ class RecentWork extends Component {
 						<h2 class="work-header text-uppercase">Recent Work</h2>
 					</div>
 				</div>
-				<div className="row work-border py-5 mb-5 mx-0 mx-lg-5">
-					<div className="col-12 col-md-6">
+				<div className="row work-border mb-5 mx-0 mx-lg-5">
+					<div className="col-12 col-md-6 py-5 semihandmade-col">
 					 	<img className="work-img img-fluid" src={Semihandmade} alt="semihandmade"/>
 					</div>
-					<div className="col-12 col-md-6 pt-5 pt-md-0">
+					<div className="col-12 col-md-6 py-5">
 						<h3 className="text-left text-uppercase position-header">Front End Development + Design</h3>
 						<h2 className="text-left work-title">Semihandmade</h2>
 						<p className="text-left m-0 work-paragraph">Developed and implemented front end features for Semihandmade, a Shopify Plus client that makes custom doors for Ikea kitchen cabinets, bathroom, media and storage systems.</p>
 
 					</div>
 				</div>
-				<div className="row work-border py-5 mb-5 mx-0 mx-lg-5">
-					<div className="col-12 col-md-6">
+				<div className="row work-border mb-5 mx-0 mx-lg-5">
+					<div className="col-12 col-md-6 py-5 laloop-col">
 						<img className="work-img img-fluid" src={Laloop} alt="laloop"/>
 					</div>
-					<div className="col-12 col-md-6 pt-5 pt-md-0">
+					<div className="col-12 col-md-6 py-5">
 						<h3 className="text-left text-uppercase position-header">Front End Development + Design</h3>
 						<h2 className="text-left work-title">La Loop</h2>
 						<p className="text-left m-0 work-paragraph">Developed and implemented front end features for La Loop, a Shopify client that creates and designs eyewear accessories for users. </p>
 					</div>
 				</div>
-				<div className="row work-border py-5 mb-5 mx-0 mx-lg-5">
-					<div className="col-12 col-md-6">
+				<div className="row work-border mb-5 mx-0 mx-lg-5">
+					<div className="col-12 col-md-6 py-5 ymy-col">
 						<img className="work-img img-fluid" src={Ymy} alt="ymy"/>
 					</div>
-					<div className="col-12 col-md-6 pt-5 pt-md-0">
+					<div className="col-12 col-md-6 py-5">
 						<h3 className="text-left text-uppercase position-header">Front End Development + Design</h3>
 						<h2 className="text-left work-title">Yamamotoyama</h2>
 						<p className="text-left m-0 work-paragraph">Developed and implemented front end features for Yamamotoyama USA, a Shopify client that specializes in providing the finest teas and nori all over the world.</p>
 					</div>
 				</div>
-				<div className="row work-border py-5 mb-5 mx-0 mx-lg-5">
-					<div className="col-12 col-md-6">
+				<div className="row work-border mb-5 mx-0 mx-lg-5">
+					<div className="col-12 col-md-6 py-5 singnasium-col">
 						<img className="work-img img-fluid" src={Singnasium} alt="singnasium"/>
 					</div>
-					<div className="col-12 col-md-6 pt-5 pt-md-0">
+					<div className="col-12 col-md-6 py-5">
 						<h3 className="text-left text-uppercase position-header">Front End Development + Design</h3>
 						<h2 className="text-left work-title">Singnasium</h2>
 						<p className="text-left m-0 work-paragraph">Developed and implemented front end features for Singnasium, a Shopify client that provides safe, non judgmental place for NYC artists and singers to explore their creative selves.</p>
