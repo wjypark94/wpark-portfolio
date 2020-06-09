c1420e784ca850b2a0832b2ef8b4a425806cb2f2
William Park <williampark@Williams-MBP-2.lan>
2020-06-09 16:04:02 -0700

    added basic descriptions

--- src/components/PastProjects.js
+++ src/components/PastProjects.js
@@ -16,25 +16,25 @@ class PastProjects extends Component{
 				</div>
 
 				<div className="row mx-0 mx-lg-5 mb-5">
-					<div className="col-6">
-						<div className="row work-border mr-1">
+					<div className="col-12 col-md-6">
+						<div className="row work-border mr-md-1">
 							<div className="col-12 px-0">
 								<img className="img-fluid" src={Fridgeful} alt="fridgeful"/>
 							</div>
 							<div className="col-12">
 								<h2 className="work-title">Fridgeful</h2>
-								<p className="work-paragraph">App description</p>
+								<p className="work-paragraph">Helping users decrease the amount of food waste in the world, Fridgeful allows users to search recipes based on the ingredients in their fridge. Users are able to write comments and save recipes into their own personal database.</p>
 							</div>
 						</div>
 					</div>
-					<div className="col-6">
-						<div className="row work-border ml-1">
+					<div className="col-12 col-md-6">
+						<div className="row work-border ml-md-1">
 							<div className="col-12 px-0">
 								<img className="img-fluid" src={Brewtrackr} alt="brewtrackr"/>
 							</div>
 							<div className="col-12">
 								<h2 className="work-title">BrewTrackr</h2>
-								<p className="work-paragraph">App description</p>
+								<p className="work-paragraph">BrewTrackr allows users to search for coffee or breweries in any city. Users are able to write their own reviews and save their search results into their own list. Keep track of all your favorite coffee and breweries with BrewTrackr.</p>
 							</div>
 						</div>
 					</div>
@@ -42,25 +42,25 @@ class PastProjects extends Component{
 
 
 				<div className="row mx-0 mx-lg-5 mb-5">
-					<div className="col-6">
-						<div className="row work-border mr-1">
+					<div className="col-12 col-md-6">
+						<div className="row work-border mr-md-1">
 							<div className="col-12 px-0">
 								<img className="img-fluid" src={HotCold} alt="hotcold"/>
 							</div>
 							<div className="col-12">
 								<h2 className="work-title">Hot N' Cold</h2>
-								<p className="work-paragraph">App description</p>
+								<p className="work-paragraph">Built with React and Redux, Hot or Cold is an interactive and simple number guessing game. The app allows users to attempt to guess a randomly generated number with cold or hot hints throughout the game.</p>
 							</div>
 						</div>
 					</div>
-					<div className="col-6">
-						<div className="row work-border ml-1">
+					<div className="col-12 col-md-6">
+						<div className="row work-border ml-md-1">
 							<div className="col-12">
 								<img className="img-fluid" src={TrekCheck} alt="trekcheck"/>
 							</div>
 							<div className="col-12">
 								<h2 className="work-title">TrekCheck</h2>
-								<p className="work-paragraph">App description</p>
+								<p className="work-paragraph">TrekCheck is a responsive web application that allows users to look up both hiking and weather data in a specified location with one quick search. Check the current weather and find the best hiking trails nearby with TrekCheck.</p>
 							</div>
 						</div>
 					</div>
