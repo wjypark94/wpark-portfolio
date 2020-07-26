3779c7faf0ee8bb17dc73ec18b9029449d2d0a2e
William Park <williampark@Williams-MBP-2-29.lan>
2020-07-25 22:34:10 -0700

    added sample icons for header

--- src/components/Header.js
+++ src/components/Header.js
@@ -22,7 +22,9 @@ class Header extends Component {
 					</div>
 
 					<div class="col-12">
-
+						<a class="px-3" href="mailto:wjypark94@gmail.com"><i class="icon fa fa-envelope"></i></a>
+						<a class="px-3" href="https://github.com/wjypark94"><i class="icon fa fa-github-alt"></i></a>
+						<a class="px-3" href="https://www.linkedin.com/in/wjypark"><i class="icon fa fa-linkedin"></i></a>
 					</div>
 					 
 					<div class="col-12 py-3">
