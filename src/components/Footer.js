f1cdbf4fcc3f22d67cc9f844fa21cc2e6db00543
William Park <williampark@Williams-MBP-2-36.lan>
2020-08-12 16:01:19 -0700

    added styles to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,10 +5,10 @@ class Footer extends Component{
 		return (
 			<div class="container py-3 py-lg-5">
 				<div class="row mx-0 mx-lg-5">
-					<div class="col-12 text-center text-md-left pl-0 py-3">
+					<div class="col-12 text-center text-md-left pl-0">
 						<h2 class="contact-header text-uppercase">Contact</h2>
 					</div>
-					<div class="col-12 text-center">
+					<div class="col-12 text-center py-3 py-lg-4">
 						<h3>Let's connect!</h3>
 						<p class="m-0">Learn more <a href="/about">about me</a> or feel free to reach out to chat!</p>
 						<p class="m-0">Living, learning, and leveling up one day at a time.</p>
@@ -17,7 +17,7 @@ class Footer extends Component{
 						<a class="px-3" href="mailto:wjypark94@gmail.com"><i class="icon fa fa-envelope"></i></a>
 						<a class="px-3" href="https://github.com/wjypark94"><i class="icon fa fa-github-alt"></i></a>
 						<a class="px-3" href="https://www.linkedin.com/in/wjypark"><i class="icon fa fa-linkedin"></i></a>
-						<p>2020 William Park</p>
+						<p className="py-4">2020 - William Park</p>
 					</div>
 
 				</div>
