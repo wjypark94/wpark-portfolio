fcd560ca4095dcb77b23da93e2bfdd3f5fdfba9d
William Park <williampark@Williams-MacBook-Pro-2.local>
2020-09-16 20:00:59 -0700

    added text to project titles and descriptions

--- src/components/PastProjects.js
+++ src/components/PastProjects.js
@@ -20,12 +20,16 @@ class PastProjects extends Component{
 							<div class="card-body">
 								<img className="img-fluid project-image" src={Fridgeful} alt="fridgeful"/>
 								<div class="overlay w-100 px-3 d-flex align-items-center justify-content-center">
-									<div class="row w-100">
-										<div class="col-12 col-lg-6 py-2">
-											<a href="https://fridgeful.netlify.com/" class="btn btn-primary w-100 project-btn" title="Fridgeful Demo" target="_blank">Live</a>
+									<div class="row">
+										<div className="col-12 py-2 title-col">
+											<h2 className="project-title">Fridgeful</h2>
+											<p className="project-description mb-0">Decrease food waste and search recipes based on what's in your fridge</p>
 										</div>
-										<div class="col-12 col-lg-6 py-2">
-											<a href="https://github.com/wjypark94/fridgeful-client" class="btn btn-primary w-100 project-btn" title="Fridgeful Code" target="_blank">Github</a>
+										<div class="col-6 py-2 button-col">
+											<a href="https://fridgeful.netlify.com/" class="btn btn-primary project-btn" title="Fridgeful Demo" target="_blank">Live</a>
+										</div>
+										<div class="col-6 py-2 button-col">
+											<a href="https://github.com/wjypark94/fridgeful-client" class="btn btn-primary project-btn" title="Fridgeful Code" target="_blank">Github</a>
 										</div>
 									</div>
 								</div>
@@ -37,12 +41,16 @@ class PastProjects extends Component{
 							<div class="card-body">
 								<img className="img-fluid project-image" src={Brewtrackr} alt="brewtrackr"/>
 								<div class="overlay w-100 px-3 d-flex align-items-center justify-content-center">
-									<div class="row w-100">
-										<div class="col-12 col-lg-6 py-2">
-											<a href="https://brewtrackr.herokuapp.com/" class="btn btn-primary w-100 project-btn" title="Brewtrackr Demo" target="_blank">Live</a>
+									<div class="row">
+										<div className="col-12 py-2 title-col">
+											<h2 className="project-title">Brewtrackr</h2>
+											<p className="project-description mb-0">Keep track of all your favorite coffee and breweries in a city</p>
 										</div>
-										<div class="col-12 col-lg-6 py-2">
-											<a href="https://github.com/wjypark94/brew-trackr" class="btn btn-primary w-100 project-btn" title="Brewtrackr Code" target="_blank">Github</a>
+										<div class="col-6 py-2 button-col">
+											<a href="https://brewtrackr.herokuapp.com/" class="btn btn-primary project-btn" title="Brewtrackr Demo" target="_blank">Live</a>
+										</div>
+										<div class="col-6 py-2 button-col">
+											<a href="https://github.com/wjypark94/brew-trackr" class="btn btn-primary project-btn" title="Brewtrackr Code" target="_blank">Github</a>
 										</div>
 									</div>
 								</div>
@@ -54,12 +62,16 @@ class PastProjects extends Component{
 							<div class="card-body">
 								<img className="img-fluid project-image" src={HotCold} alt="hotcold"/>	
 								<div class="overlay w-100 px-3 d-flex align-items-center justify-content-center">
-									<div class="row w-100">
-										<div class="col-12 col-lg-6 py-2">
-											<a href="https://hotcold.netlify.app/" class="btn btn-primary w-100 project-btn" title="Hotcold Demo" target="_blank">Live</a>
+									<div class="row">
+										<div className="col-12 py-2 title-col">
+											<h2 className="project-title">Hot n Cold</h2>
+											<p className="project-description mb-0">An interactive and simple number guessing game built with React and Redux</p>
 										</div>
-										<div class="col-12 col-lg-6 py-2">
-											<a href="https://github.com/wjypark94/hot-cold" class="btn btn-primary w-100 project-btn" title="Hotcold Code" target="_blank">Github</a>
+										<div class="col-6 py-2 button-col">
+											<a href="https://hotcold.netlify.app/" class="btn btn-primary project-btn" title="Hotcold Demo" target="_blank">Live</a>
+										</div>
+										<div class="col-6 py-2 button-col">
+											<a href="https://github.com/wjypark94/hot-cold" class="btn btn-primary project-btn" title="Hotcold Code" target="_blank">Github</a>
 										</div>
 									</div>
 								</div>
@@ -71,12 +83,16 @@ class PastProjects extends Component{
 							<div class="card-body">
 								<img className="img-fluid project-image" src={TrekCheck} alt="trekcheck"/>
 								<div class="overlay w-100 px-3 d-flex align-items-center justify-content-center">
-									<div class="row w-100">
-										<div class="col-12 col-lg-6 py-2">
-											<a href="https://wjypark94.github.io/trek-check/" class="btn btn-primary w-100 project-btn" title="Trekcheck Demo" target="_blank">Live</a>
+									<div class="row">
+										<div className="col-12 py-2 title-col">
+											<h2 className="project-title">TrekCheck</h2>
+											<p className="project-description mb-0">Check both the weather and nearby hiking trails with one quick search</p>
 										</div>
-										<div class="col-12 col-lg-6 py-2">
-											<a href="https://github.com/wjypark94/trek-check" class="btn btn-primary w-100 project-btn" title="Trekcheck Code" target="_blank">Github</a>
+										<div class="col-6 py-2 button-col">
+											<a href="https://wjypark94.github.io/trek-check/" class="btn btn-primary project-btn" title="Trekcheck Demo" target="_blank">Live</a>
+										</div>
+										<div class="col-6 py-2 button-col">
+											<a href="https://github.com/wjypark94/trek-check" class="btn btn-primary project-btn" title="Trekcheck Code" target="_blank">Github</a>
 										</div>
 									</div>
 								</div>
