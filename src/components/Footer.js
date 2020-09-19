67ba3818bf38e412264621d43af080dbef52b40c
William Park <williampark@Williams-MBP-2.lan>
2020-09-18 17:56:24 -0700

    fixed responsive footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,15 +5,13 @@ import Fade from 'react-reveal/Fade';
 class Footer extends Component{
 	render(){
 		return (
-			<div class="container py-3 py-lg-5">
+			<div class="container py-3">
 				<div class="row mx-0 mx-lg-5">
 					<div class="col-12 text-center text-md-left pl-0">
-						<h2 class="contact-header text-uppercase py-3">
-							<Fade top>Contact</Fade>
-						</h2>
+						<h2 class="contact-header text-uppercase py-3"><Fade top>Contact</Fade></h2>
 					</div>
 					<div class="col-12 text-center py-4 py-lg-5">
-						<h3 className="footer-title">Get In Touch</h3>
+						<h3 className="footer-title"><Fade left>Get In Touch</Fade></h3>
 						<p class="m-0 footer-description pt-3 pt-lg-4">Learn more <a href="/about" className="contact-link">about me</a> or feel free to reach out to chat!</p>
 						<p class="m-0 footer-description">Living, learning, and leveling up one day at a time.</p>
 					</div>
@@ -25,9 +23,8 @@ class Footer extends Component{
 						<p className="pt-4 footer-copyright"><i class="icon fa fa-copyright copyright-icon"></i>2020 - William Park</p>
 						</Fade>
 					</div>
-
 				</div>
-				
+			
 			</div>
 		)
 	}
