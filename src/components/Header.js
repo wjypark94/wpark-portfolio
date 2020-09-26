cad70573f3eec819197c11e36baf1b0c4229eb7f
William Park <williampark@Williams-MacBook-Pro-2.local>
2020-09-25 21:19:44 -0700

    added react typical

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,7 @@ import Scroll from 'react-scroll';
 import Fade from 'react-reveal/Fade';
 import Avatar from '../images/avatar.png';
 import Ymy from '../images/ymy.png';
+import Typical from 'react-typical'
 
 class Header extends Component {
 	render() {
@@ -15,8 +16,15 @@ class Header extends Component {
 					<div className="row pt-3 pt-lg-4">
 						<div className="col-12 py-3 header-title">
 							<Fade top>
+
 							<h2>Hi, I'm <span>Will Park</span></h2>
-							<h3>I'm a Front End Web Developer based in Los Angeles.</h3>
+	
+							<h3>I'm a{' '}					
+							<Typical
+							steps={['Frontend Developer', 1500, 'Creative Designer', 1500, 'Collaborator', 1500, 'Photographer', 1500]}
+							loop={Infinity}
+							wrapper="span"
+							/></h3>
 							</Fade>
 						</div>
 						<div class="col-12 text-center">
@@ -29,6 +37,18 @@ class Header extends Component {
 							<a class="px-2" href="https://github.com/wjypark94" target="_blank"><i class="icon fa fa-github-alt"></i></a>
 							<a class="px-2" href="https://www.linkedin.com/in/wjypark" target="_blank"><i class="icon fa fa-linkedin"></i></a>
 						</div>
+					</div>
+					<div className="row justify-content-center pt-5">
+						<div classNamee="col-12">
+							<div className="arrow-wrapper">
+								<Fade bottom>
+									<Link to="background" offset={-40} spy={true} smooth={true} duration={200}>
+									<i className="fa fa-chevron-down smooth-container" aria-hidden="true" ></i>
+									</Link>
+								</Fade>
+							</div>
+						</div>
+
 					</div>
 				</div>
 			</section>
